85eb8c2f0bddc6e566818f0185dec92494963052
Alex McKinney <mensch@uber.com>
2018-10-03 16:51:46 -0700

    Add gRPC reference

--- v2/yarpcprotobuf/protoc-gen-yarpc-go/internal/testdata/key_value.pb.go
+++ v2/yarpcprotobuf/protoc-gen-yarpc-go/internal/testdata/key_value.pb.go
@@ -7,6 +7,11 @@ import proto "github.com/golang/protobuf/proto"
 import fmt "fmt"
 import math "math"
 
+import (
+	context "golang.org/x/net/context"
+	grpc "google.golang.org/grpc"
+)
+
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
 var _ = fmt.Errorf
@@ -81,6 +86,271 @@ var xxx_messageInfo_GetResponse proto.InternalMessageInfo
 func init() {
 	proto.RegisterType((*GetRequest)(nil), "keyvalue.GetRequest")
 	proto.RegisterType((*GetResponse)(nil), "keyvalue.GetResponse")
+}
+
+// Reference imports to suppress errors if they are not otherwise used.
+var _ context.Context
+var _ grpc.ClientConn
+
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the grpc package it is being compiled against.
+const _ = grpc.SupportPackageIsVersion4
+
+// KeyValueClient is the client API for KeyValue service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
+type KeyValueClient interface {
+	Foo(ctx context.Context, in *GetRequest, opts ...grpc.CallOption) (*GetResponse, error)
+	Bar(ctx context.Context, opts ...grpc.CallOption) (KeyValue_BarClient, error)
+	Baz(ctx context.Context, in *GetRequest, opts ...grpc.CallOption) (KeyValue_BazClient, error)
+	Qux(ctx context.Context, opts ...grpc.CallOption) (KeyValue_QuxClient, error)
+}
+
+type keyValueClient struct {
+	cc *grpc.ClientConn
+}
+
+func NewKeyValueClient(cc *grpc.ClientConn) KeyValueClient {
+	return &keyValueClient{cc}
+}
+
+func (c *keyValueClient) Foo(ctx context.Context, in *GetRequest, opts ...grpc.CallOption) (*GetResponse, error) {
+	out := new(GetResponse)
+	err := c.cc.Invoke(ctx, "/keyvalue.KeyValue/Foo", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *keyValueClient) Bar(ctx context.Context, opts ...grpc.CallOption) (KeyValue_BarClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_KeyValue_serviceDesc.Streams[0], "/keyvalue.KeyValue/Bar", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &keyValueBarClient{stream}
+	return x, nil
+}
+
+type KeyValue_BarClient interface {
+	Send(*GetRequest) error
+	CloseAndRecv() (*GetResponse, error)
+	grpc.ClientStream
+}
+
+type keyValueBarClient struct {
+	grpc.ClientStream
+}
+
+func (x *keyValueBarClient) Send(m *GetRequest) error {
+	return x.ClientStream.SendMsg(m)
+}
+
+func (x *keyValueBarClient) CloseAndRecv() (*GetResponse, error) {
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	m := new(GetResponse)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func (c *keyValueClient) Baz(ctx context.Context, in *GetRequest, opts ...grpc.CallOption) (KeyValue_BazClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_KeyValue_serviceDesc.Streams[1], "/keyvalue.KeyValue/Baz", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &keyValueBazClient{stream}
+	if err := x.ClientStream.SendMsg(in); err != nil {
+		return nil, err
+	}
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	return x, nil
+}
+
+type KeyValue_BazClient interface {
+	Recv() (*GetResponse, error)
+	grpc.ClientStream
+}
+
+type keyValueBazClient struct {
+	grpc.ClientStream
+}
+
+func (x *keyValueBazClient) Recv() (*GetResponse, error) {
+	m := new(GetResponse)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func (c *keyValueClient) Qux(ctx context.Context, opts ...grpc.CallOption) (KeyValue_QuxClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_KeyValue_serviceDesc.Streams[2], "/keyvalue.KeyValue/Qux", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &keyValueQuxClient{stream}
+	return x, nil
+}
+
+type KeyValue_QuxClient interface {
+	Send(*GetRequest) error
+	Recv() (*GetResponse, error)
+	grpc.ClientStream
+}
+
+type keyValueQuxClient struct {
+	grpc.ClientStream
+}
+
+func (x *keyValueQuxClient) Send(m *GetRequest) error {
+	return x.ClientStream.SendMsg(m)
+}
+
+func (x *keyValueQuxClient) Recv() (*GetResponse, error) {
+	m := new(GetResponse)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+// KeyValueServer is the server API for KeyValue service.
+type KeyValueServer interface {
+	Foo(context.Context, *GetRequest) (*GetResponse, error)
+	Bar(KeyValue_BarServer) error
+	Baz(*GetRequest, KeyValue_BazServer) error
+	Qux(KeyValue_QuxServer) error
+}
+
+func RegisterKeyValueServer(s *grpc.Server, srv KeyValueServer) {
+	s.RegisterService(&_KeyValue_serviceDesc, srv)
+}
+
+func _KeyValue_Foo_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(KeyValueServer).Foo(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/keyvalue.KeyValue/Foo",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(KeyValueServer).Foo(ctx, req.(*GetRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _KeyValue_Bar_Handler(srv interface{}, stream grpc.ServerStream) error {
+	return srv.(KeyValueServer).Bar(&keyValueBarServer{stream})
+}
+
+type KeyValue_BarServer interface {
+	SendAndClose(*GetResponse) error
+	Recv() (*GetRequest, error)
+	grpc.ServerStream
+}
+
+type keyValueBarServer struct {
+	grpc.ServerStream
+}
+
+func (x *keyValueBarServer) SendAndClose(m *GetResponse) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func (x *keyValueBarServer) Recv() (*GetRequest, error) {
+	m := new(GetRequest)
+	if err := x.ServerStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func _KeyValue_Baz_Handler(srv interface{}, stream grpc.ServerStream) error {
+	m := new(GetRequest)
+	if err := stream.RecvMsg(m); err != nil {
+		return err
+	}
+	return srv.(KeyValueServer).Baz(m, &keyValueBazServer{stream})
+}
+
+type KeyValue_BazServer interface {
+	Send(*GetResponse) error
+	grpc.ServerStream
+}
+
+type keyValueBazServer struct {
+	grpc.ServerStream
+}
+
+func (x *keyValueBazServer) Send(m *GetResponse) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func _KeyValue_Qux_Handler(srv interface{}, stream grpc.ServerStream) error {
+	return srv.(KeyValueServer).Qux(&keyValueQuxServer{stream})
+}
+
+type KeyValue_QuxServer interface {
+	Send(*GetResponse) error
+	Recv() (*GetRequest, error)
+	grpc.ServerStream
+}
+
+type keyValueQuxServer struct {
+	grpc.ServerStream
+}
+
+func (x *keyValueQuxServer) Send(m *GetResponse) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func (x *keyValueQuxServer) Recv() (*GetRequest, error) {
+	m := new(GetRequest)
+	if err := x.ServerStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+var _KeyValue_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "keyvalue.KeyValue",
+	HandlerType: (*KeyValueServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "Foo",
+			Handler:    _KeyValue_Foo_Handler,
+		},
+	},
+	Streams: []grpc.StreamDesc{
+		{
+			StreamName:    "Bar",
+			Handler:       _KeyValue_Bar_Handler,
+			ClientStreams: true,
+		},
+		{
+			StreamName:    "Baz",
+			Handler:       _KeyValue_Baz_Handler,
+			ServerStreams: true,
+		},
+		{
+			StreamName:    "Qux",
+			Handler:       _KeyValue_Qux_Handler,
+			ServerStreams: true,
+			ClientStreams: true,
+		},
+	},
+	Metadata: "key_value.proto",
 }
 
 func init() { proto.RegisterFile("key_value.proto", fileDescriptor_key_value_6105a7c652e4cf0c) }
