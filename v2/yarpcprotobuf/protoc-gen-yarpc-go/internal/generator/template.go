ce0318dd6f99a18bb0a38be4586b934aa1645e40
Alex McKinney <mensch@uber.com>
2018-10-02 19:00:11 -0700

    Add goType method

--- v2/yarpcprotobuf/protoc-gen-yarpc-go/internal/generator/template.go
+++ v2/yarpcprotobuf/protoc-gen-yarpc-go/internal/generator/template.go
@@ -2,6 +2,7 @@ package generator
 
 import (
 	"bytes"
+	"fmt"
 	"text/template"
 )
 
@@ -14,7 +15,11 @@ var _tmpl = template.Must(
 )
 
 func parseTemplates(templates ...string) (*template.Template, error) {
-	t := template.New(_plugin)
+	t := template.New(_plugin).Funcs(
+		template.FuncMap{
+			"goType": goType,
+		},
+	)
 	for _, tmpl := range templates {
 		_, err := t.Parse(tmpl)
 		if err != nil {
@@ -30,4 +35,18 @@ func execTemplate(data interface{}) ([]byte, error) {
 		return nil, err
 	}
 	return buffer.Bytes(), nil
+}
+
+// goType returns a go type name for the message type.
+// It prefixes the type name with the package's alias
+// if it does not belong to the same package.
+//
+//  Ex:
+//  *Foo
+//  *foo.Foo
+func goType(m *Message, pkg string) string {
+	if m.Package.GoPackage == pkg {
+		return m.Name
+	}
+	return fmt.Sprintf("%s.%s", m.Package.Alias, m.Name)
 }
