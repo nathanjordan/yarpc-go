5de0921ce8209de31a5337e13341e756b2d5a0da
Vyom Shah <vyom@uber.com>
2018-10-11 16:26:25 -0700

    Construct MapRouter with EncodingProcedures

--- v2/yarpcrouter/router.go
+++ v2/yarpcrouter/router.go
@@ -63,6 +63,53 @@ func NewMapRouter(defaultService string) MapRouter {
 	}
 }
 
+// NewMapRouterWithEncodingProcedures constructs a new MapRouter with the given default service name and registers
+// the given encoding level procedures as transport level procedures
+func NewMapRouterWithEncodingProcedures(defaultService string, encodingProcedures []yarpc.EncodingProcedure) MapRouter {
+	router := MapRouter{
+		defaultService:            defaultService,
+		serviceProcedureEncodings: make(map[serviceProcedureEncoding]yarpc.TransportProcedure),
+		serviceNames:              map[string]struct{}{defaultService: {}},
+	}
+
+	transportProcedures := []yarpc.TransportProcedure{}
+	for _, p := range encodingProcedures {
+		handler := func(c context.Context, r *yarpc.Request, b *yarpc.Buffer) (*yarpc.Response, *yarpc.Buffer, error) {
+			decodedBody, codecErr := p.Codec.Decode(b)
+			if codecErr != nil {
+				return nil, nil, codecErr
+			}
+
+			response, body, err := p.HandlerSpec.Unary().Handle(c, r, decodedBody)
+			if err != nil {
+				return nil, nil, err
+			}
+
+			encodedBody, codecErr := p.Codec.Encode(body)
+			if codecErr != nil {
+				return nil, nil, codecErr
+			}
+
+			return response, encodedBody, nil
+		}
+
+		transportProcedures = append(
+			transportProcedures,
+			yarpc.TransportProcedure{
+				Name:        p.Name,
+				Service:     p.Service,
+				HandlerSpec: yarpc.NewUnaryTransportHandlerSpec(yarpc.UnaryTransportHandlerFunc(handler)),
+				Encoding:    p.Encoding,
+				Signature:   p.Signature,
+			},
+		)
+
+		router.Register(transportProcedures)
+	}
+
+	return router
+}
+
 // Register registers the procedure with the MapRouter.
 // If the procedure does not specify its service name, the procedure will
 // inherit the default service name of the router.
